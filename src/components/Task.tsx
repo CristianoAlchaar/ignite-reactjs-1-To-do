d2fcfaf40d03274bb254103ea84729b4c771fd53
Cristiano Silva <alchaarcristiano@gmail.com>
2023-02-07 21:47:10 +0000

    Fix Task component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -19,31 +19,18 @@ export function Task(props:TaskProps){
     }
 
     function handleChangeDoneTask(){
+        setIsChecked(!isChecked);
         props.onChangeDoneValue(props.id);
     }
 
-    if(isChecked){  
-        return(
-            <div className={styles.Task}>
-                    <label className={styles.lblContainer}>
-                        <input type="checkbox" onChange={handleChangeDoneTask} checked={isChecked}/>
-                        <span className={styles.checkmark}></span>
-                    </label>
-                    <p className={styles.disabled}>{props.description}</p> 
-                    <img className={styles.trashIcon} src={trashIcon} onClick={handleDeleteTask}></img>
-                </div>
-        )
-    }
-    else{ 
-        return(
-            <div className={styles.Task}>
-                <label className={styles.lblContainer}>
-                    <input type="checkbox" onChange={handleChangeDoneTask}/>
-                    <span className={styles.checkmark}></span>
-                </label>
-                <p>{props.description}</p> 
-                <img className={styles.trashIcon} src={trashIcon} onClick={handleDeleteTask}></img>
-            </div>
-        )
-    }       
+    return(
+        <div className={styles.Task}>
+            <label className={styles.lblContainer}>
+                <input type="checkbox" onChange={handleChangeDoneTask} checked={isChecked}/>
+                <span className={styles.checkmark}></span>
+            </label>
+            <p className={isChecked ? styles.disabled : ''}>{props.description}</p> 
+            <img className={styles.trashIcon} src={trashIcon} onClick={handleDeleteTask}></img>
+        </div>
+    )       
 }
